8de8cd4276118b39fc40647a2767657911ec395f
Chuck LeDuc Díaz <celeduc@gmail.com>
2016-07-12 00:59:30 +0200

    Jamie's patch

--- lib/ethereum/ipc_client.rb
+++ lib/ethereum/ipc_client.rb
@@ -1,35 +1,62 @@
 require 'socket'
 module Ethereum
   class IpcClient < Client
-    attr_accessor :ipcpath
+    attr_accessor :command, :id, :ipcpath, :batch, :converted_transactions, :log, :logger
 
-    def initialize(ipcpath = "#{ENV['HOME']}/.ethereum/geth.ipc", log = false)
-      super(log)
+    def initialize(ipcpath = "#{ENV['HOME']}/.ethereum/geth.ipc", log = true)
       @ipcpath = ipcpath
+      @id = 1
+      @batch = []
+      @log = log
+      if @log == true
+        @logger = Logger.new("/tmp/ethereum_ruby_ipc.log")
+      end
     end
 
-    def send_single(payload)
+    RPC_COMMANDS.each do |rpc_command|
+      method_name = "#{rpc_command.split("_")[1].underscore}"
+      define_method method_name do |*args|
+        command = rpc_command
+        if command == "eth_call"
+          args << "latest"
+        end
+        payload = {jsonrpc: "2.0", method: command, params: args, id: get_id}
+        socket = UNIXSocket.new(@ipcpath)
+        socket.puts(payload.to_json)
+        if @log == true
+          @logger.info("Sending #{payload.to_json}")
+        end
+        read = socket.gets
+        socket.close
+	if @log == true
+	  @logger.info("Received #{read}")
+	end
+        output = JSON.parse(read)
+        return output
+      end
+
+      define_method "#{method_name}_batch" do |*args|
+        command = rpc_command
+        payload = {jsonrpc: "2.0", method: command, params: args, id: get_id}
+        @batch << payload.to_json
+      end
+    end
+
+    def send_batch
       socket = UNIXSocket.new(@ipcpath)
-      socket.puts(payload)
-      read = socket.gets
-      socket.close
-      return read
+      socket.write(@batch.join(" "))
+      socket.close_write
+      read = socket.read
+      collection = read.chop.split("}{").collect do |output|
+        if output[0] == "{"
+          JSON.parse("#{output}}")["result"]
+        else
+          JSON.parse("{#{output}}")["result"]
+        end
+      end
+      return collection
     end
 
-    # TODO: Not sure if multithread safe
-    # Note: Guarantees the results are in the same order as defined in batch call.
-    # client.batch do
-    #   client.eth_block_number
-    #   client.eth_mining
-    # end
-    # => [{"jsonrpc"=>"2.0", "id"=>1, "result"=>"0x26"}, {"jsonrpc"=>"2.0", "id"=>2, "result"=>false}] 
-    def send_batch(batch)
-      result = send_single(batch.to_json)
-      result = JSON.parse(result)
-
-      # Make sure the order is the same as it was when batching calls
-      # See 6 Batch here http://www.jsonrpc.org/specification
-      return result.sort_by! { |c| c['id'] }
-    end
   end
 end
+
